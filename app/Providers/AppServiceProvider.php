8c0c64ac77fedfe90b90343d7c90f1e018fa7a26
gercova <germancotrina17@gmail.com>
2026-09-20 18:04:47 -0500

    feat: método boot modificado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Business;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer(['admin.layout'], function ($view) {
+            $view->with('business', Business::first());
+        });
     }
 }
